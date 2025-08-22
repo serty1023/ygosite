1a2ae918a8f41ef9f9ff6b5fc0c67dda4c95ede8
Mai Luong Bac <serty1023@Mais-MacBook-Pro.local>
2025-08-22 22:12:39 +0700

    ygo

--- client/source/components/decks.jsx
+++ client/source/components/decks.jsx
@@ -1,11 +1,17 @@
-import "../style/decks.css"
+import "../style/decks.css";
+import deckData from "/Users/serty1023/Documents/YGOSITE/server/data/decks.json";
 
 function Decks()
 {
     return (
     <>
         <div className="decks-container">
-            
+            {deckData.decks.map(deck => 
+            (
+                <h1>
+                    {deck.name}
+                </h1>
+            ))}
         </div>
     </>);
 };
