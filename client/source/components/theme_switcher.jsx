10fc7b83862467f36205a33ae2fbde70f8518bda
Mai Luong Bac <serty1023@Mais-MacBook-Pro.local>
2025-08-28 22:06:24 +0700

    ygo

--- client/source/components/theme_switcher.jsx
+++ client/source/components/theme_switcher.jsx
@@ -1,13 +1,16 @@
+import { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { ThemeContext } from "./theme";
 import "../style/header.css";
 
 function ThemeSwitcher()
 {
+    const { theme,setTheme } = useContext(ThemeContext);
     return (
     <>
         <div className="dark-mode">
-            <FontAwesomeIcon className="icon" icon={faMoon}/>
+            <FontAwesomeIcon className="icon" icon={faMoon} onClick={() => setTheme(theme === "dark" ? "light" : "dark")}/>
         </div>
     </>);
 };
