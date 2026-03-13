d03252a118f4081ce5477f212e13b2bce2ed4a58
Mai Luong Bac <serty1023@Mais-MacBook-Pro.local>
2026-03-13 20:50:45 +0700

    ygo

--- client/source/components/tutorial.jsx
+++ client/source/components/tutorial.jsx
@@ -9,7 +9,7 @@ function Tutorials()
 
     useEffect(() =>
     {
-        fetch("http://localhost:3000/interface/tutorials",
+        fetch(`http://localhost:3000/interface/tutorials/${language}`,
         {
             headers: {"access":"true"}
         })
--- server/routes/interface.js
+++ server/routes/interface.js
@@ -2,7 +2,6 @@ const express = require("express");
 const interfaceRouter = express.Router();
 const header = require("../data/header.json");
 const introduction = require("../data/introduction.json");
-const tutorials = require("../data/tutorials/tutorials.json");
 const search_options = require("../data/search_options.json");
 const filterbar = require("../data/filterbar.json");
 const card_layout = require("../data/card_layout.json");
@@ -11,13 +10,13 @@ const developerCheck = require("../controller/authorize");
 
 interfaceRouter.get("/interface", developerCheck, (request,response) =>
 {
-    response.json( {header,introduction,tutorials,search_options,filterbar,card_layout,footer} );
+    response.json( {header,introduction,search_options,filterbar,card_layout,footer} );
 });
 
 interfaceRouter.get("/interface/:name", developerCheck, (request,response) =>
 {
     const { name } = request.params;
-    const data = {header,introduction,tutorials,search_options,filterbar,card_layout,footer};
+    const data = {header,introduction,search_options,filterbar,card_layout,footer};
     if (data[name])
     {
         response.json( data[name][name] );
@@ -27,5 +26,20 @@ interfaceRouter.get("/interface/:name", developerCheck, (request,response) =>
         response.json({error:"Not Found"});
     };
 });
+
+interfaceRouter.get("/interface/tutorials/:lang", developerCheck, (request,response) =>
+{
+    const { lang } = request.params;
+
+    try
+    {
+        const tutorials = require(`../data/tutorials/${lang}.json`);
+        response.json(tutorials);
+    }
+    catch
+    {
+        response.json({error:"Not Found"});
+    };
+})
 
 module.exports = interfaceRouter;
