8126bdab3373dbd875aa18a7a48225878791216f
Mai Luong Bac <serty1023@Mais-MacBook-Pro.local>
2026-05-29 11:29:55 +0700

    ygo

--- client/source/components/tutorial.jsx
+++ client/source/components/tutorial.jsx
@@ -5,7 +5,7 @@ import "../style/tutorial.css";
 function Tutorials()
 {
     const { language } = useContext(LanguageContext);
-    const [tutorialsContent,getTutorialContent] = useState([]);
+    const [tutorialsContent,getTutorialContent] = useState({});
 
     useEffect(() =>
     {
@@ -14,65 +14,28 @@ function Tutorials()
             headers: {"access":"true"}
         })
         .then(response => response.json())
-        .then(data => getTutorialContent(data[language]))
+        .then(data => getTutorialContent(data))
     },[language]);
+
+    console.log(tutorialsContent.navigation);
 
     return (
     <>
-        <div className="tutorials">
-            {tutorialsContent.map(part => 
+        <div className="navigation">
+            {tutorialsContent.navigation?.map((part,index) => 
             (
-                <div key={part.id} className={`indent-${part.indent}`}>
-                    <h1>
+                <>
+                    <h1 key={index} className={part.id}>
                         {part.title}
                     </h1>
-                    {part.descriptions.map((desc,index) => 
-                    {
-                        if (desc.type == "text")
-                        {
-                            return <p key={index} dangerouslySetInnerHTML={{ __html: desc.content}}></p>;
-                        }
-                        else if (desc.type == "image")
-                        {
-                            const width = 
-                                desc.size == "small" 
-                                ? "30%"
-                                : desc.size == "medium" 
-                                ? "40%"
-                                : "60%";
-                            return (
-                                <div key={index} className="image-container">
-                                    {desc.content.map((image,i) => 
-                                    (
-                                        <img key={i} src={`/source/images/${image}.png`} style={{ width }} alt={`${image}`}/>
-                                    ))}
-                                </div>
-                            );
-                        }
-                        else if (desc.type == "icon")
-                        {
-                            return (
-                                <div key={index} className="icon-container">
-                                    {desc.content.map((icon,i) =>
-                                    (
-                                        <div key={i} className="icon-div">
-                                            <img src={`/source/images/${icon}.png`} alt={`${icon}`}/>
-                                            <p>{icon}</p>
-                                        </div>
-                                    ))}
-                                </div>
-                            );
-                        }
-                        else if (desc.type == "video")
-                        {
-                            return (
-                                <video key={index} src={`/source/videos/${desc.content}.mp4`} controls></video>
-                            );
-                        };
-                        return null;
-                    })};
-                </div>
-            ))};
+                    {part.content?.map((content,index) =>    
+                    (
+                        <h2 key={index} className={content.id}>
+                            • {content.title}
+                        </h2>
+                    ))}
+                </>
+            ))}
         </div>
     </>);
 };
